cf5f7da56d8916da02434bf1e9d9ead3081fb6e9
红领巾 <355693381@qq.com>
2015-09-21 15:37:11 +0800

    修改

--- app/src/main/java/com/example/test/testgithub/MainActivity.java
+++ app/src/main/java/com/example/test/testgithub/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String test = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
